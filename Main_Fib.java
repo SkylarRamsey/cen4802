dd49683f73ad564a451820a60cb2fa37704225b4
SkylarRamsey <47016737+SkylarRamsey@users.noreply.github.com>
2023-05-16 20:14:34 -0400

    Added javadoc to fibbify method and updated readme

---
+++ Main_Fib.java
@@ -0,0 +1,29 @@
+package fibonacci;
+
+public class Main_Fib {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int target = 10;
+		
+		System.out.println("The "  + target + 
+				" term of the fibonacci sequence is: " + fibbify(10));
+
+	}
+	
+
+	public static int fibbify(int targetTerm)
+	{
+		
+		if(targetTerm < 2)
+		{
+			return targetTerm;
+		}
+		else
+		{
+			return fibbify(targetTerm - 1) + fibbify(targetTerm - 2);
+		}
+	}
+
+}
